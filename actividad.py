e47259694a77212b88c1fb814c3c56cd49ae9ea0
mizutamarilucia <mizutamarilucia@gmail.com>
2025-07-25 12:32:44 +0000

    actividad1.py

---
+++ actividad.py
@@ -0,0 +1,13 @@
+# Historia 1
+# La historia se sitúa en el pueblo ficticio de Hawkins, en el estado de Indiana, Estados Unidos, durante los años ochenta,
+# cuando un niño de doce años llamado Will Byers desaparece misteriosamente.
+# Poco después, Eleven, una niña aparentemente fugitiva y con poderes telequinéticos,
+# se encuentra con Mike, Dustin y Lucas, amigos de Will, y los ayuda en la búsqueda de su amigo desaparecido.
+
+nombrePais = str(input("Dame el nombre del pais "))
+nombreEstado = str(input("Dame el nombre del estado "))
+nombrePrincipal = str(input("Dame el mombre del personaje principal "))
+nombrePoderes = str(input("Dame el nombre de los poderes "))
+nombreChicoDesaparecido = str(input("Dame el nombre del chico desaparecido"))
+
+print()
---
+++ main.py
@@ -0,0 +1,5 @@
+# Historia 1
+# La historia se sitúa en el pueblo ficticio de Hawkins, en el estado de Indiana, Estados Unidos, durante los años ochenta,
+# cuando un niño de doce años llamado Will Byers desaparece misteriosamente.
+# Poco después, Eleven, una niña aparentemente fugitiva y con poderes telequinéticos,
+# se encuentra con Mike, Dustin y Lucas, amigos de Will, y los ayuda en la búsqueda de su amigo desaparecido.
